67e92d96304ba94d155f68e6c27fcf8853dcea70
Glaubert01 <glaubert@outlook.com>
2025-08-21 21:18:53 -0300

    .

--- lib/ui/screens/search_movies.dart
+++ lib/ui/screens/search_movies.dart
@@ -8,49 +8,56 @@ class Releases extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return FutureBuilder(
-      future: searchMoviesService.searchMovies(),
-      builder: (context, snapshot) {
-        if (snapshot.hasData) {
-          return CustomScrollView(
-            slivers: <Widget>[
-              SliverToBoxAdapter(
-                child: Image.asset("assets/popular.png", height: 80, width: 80),
-              ),
-              SliverToBoxAdapter(
-                child: Padding(
-                  padding: EdgeInsetsGeometry.only(bottom: 32),
-                  child: Center(
-                    child: Text(
-                      "Filmes Populares",
-                      style: TextStyle(
-                        fontSize: 24,
-                        fontWeight: FontWeight.bold,
+    return Scaffold(
+      backgroundColor: Colors.deepPurple,
+      body: FutureBuilder(
+        future: searchMoviesService.searchMovies(),
+        builder: (context, snapshot) {
+          if (snapshot.hasData) {
+            return CustomScrollView(
+              slivers: <Widget>[
+                SliverToBoxAdapter(
+                  child: Image.asset(
+                    "assets/popular.png",
+                    height: 80,
+                    width: 80,
+                  ),
+                ),
+                SliverToBoxAdapter(
+                  child: Padding(
+                    padding: EdgeInsetsGeometry.only(bottom: 32),
+                    child: Center(
+                      child: Text(
+                        "Filmes Populares",
+                        style: TextStyle(
+                          fontSize: 24,
+                          fontWeight: FontWeight.bold,
+                        ),
                       ),
                     ),
                   ),
                 ),
-              ),
-              SliverToBoxAdapter(
-                child: TextField(
-                  decoration: InputDecoration(
-                    border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(16),
+                SliverToBoxAdapter(
+                  child: TextField(
+                    decoration: InputDecoration(
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(16),
+                      ),
+                      hintText: "Pesquisar",
                     ),
-                    hintText: "Pesquisar",
                   ),
                 ),
-              ),
-              SliverList.builder(
-                itemBuilder: (context, index) => MovieCard(),
-                itemCount: 10,
-              ),
-            ],
-          );
-        } else {
-          return Center(child: CircularProgressIndicator());
-        }
-      },
+                SliverList.builder(
+                  itemBuilder: (context, index) => MovieCard(),
+                  itemCount: 10,
+                ),
+              ],
+            );
+          } else {
+            return Center(child: CircularProgressIndicator());
+          }
+        },
+      ),
     );
   }
 }
